7ce26edaa8d3906fb9beed115f3fae6eb2e09eef
devfsp <33924736+devfsp@users.noreply.github.com>
2018-12-03 16:37:27 +0100

    Update HelloHandlerTest.java

--- build-war/src/test/java/org/devopscon/maven/demo/HelloHandlerTest.java
+++ build-war/src/test/java/org/devopscon/maven/demo/HelloHandlerTest.java
@@ -8,6 +8,7 @@ public class HelloHandlerTest {
 	@Test
 	public void validateNameNotNull() {
 		HelloHandler handler = new HelloHandler();
+		
 		String response = handler.sayHello();
 		// Due to the code: This test will never fail :) 
 		Assert.assertNotNull("String got null value",response);
